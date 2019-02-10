5250f823b8d0bb33efd26339618faf372b165c6d
Spencer Arnold <sa.variable@gmail.com>
2019-02-09 22:16:58 -0600

    init

--- Revert-ScanSpeeder/Form1.Designer.vb
+++ Revert-ScanSpeeder/Form1.Designer.vb
@@ -22,10 +22,35 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(178, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button1 As Button
 End Class
--- Revert-ScanSpeeder/Form1.vb
+++ Revert-ScanSpeeder/Form1.vb
@@ -1,3 +1,18 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Call ShowDialog.
+        Dim result As DialogResult = OpenFileDialog1.ShowDialog()
+
+        ' Test result.
+        If result = Windows.Forms.DialogResult.OK Then
+            ' Do something.
+            Dim path As String = OpenFileDialog1.FileName
+            Dim image As Bitmap = New Bitmap(Drawing.Image.FromFile(path))
+        End If
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+
+    End Sub
 
 End Class
