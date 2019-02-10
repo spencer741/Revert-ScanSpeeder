a837243c74bdba476a2487211e911455f19d702d
Spencer Arnold <sa.variable@gmail.com>
2019-02-10 00:34:07 -0600

    Created foundation for the meat of the proj

--- Revert-ScanSpeeder/Form1.Designer.vb
+++ Revert-ScanSpeeder/Form1.Designer.vb
@@ -22,30 +22,68 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btn_BrowseFolder = New System.Windows.Forms.Button()
         Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cbo_Extensions = New System.Windows.Forms.ComboBox()
+        Me.cbo_lbl_Extensions = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'Button1
+        'btn_BrowseFolder
         '
-        Me.Button1.Location = New System.Drawing.Point(178, 12)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Browse..."
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btn_BrowseFolder.Location = New System.Drawing.Point(26, 90)
+        Me.btn_BrowseFolder.Name = "btn_BrowseFolder"
+        Me.btn_BrowseFolder.Size = New System.Drawing.Size(75, 23)
+        Me.btn_BrowseFolder.TabIndex = 0
+        Me.btn_BrowseFolder.Text = "Browse..."
+        Me.btn_BrowseFolder.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(121, 93)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'cbo_Extensions
+        '
+        Me.cbo_Extensions.FormattingEnabled = True
+        Me.cbo_Extensions.Items.AddRange(New Object() {".JPG", ".PNG", ".TIFF"})
+        Me.cbo_Extensions.Location = New System.Drawing.Point(26, 41)
+        Me.cbo_Extensions.Name = "cbo_Extensions"
+        Me.cbo_Extensions.Size = New System.Drawing.Size(121, 24)
+        Me.cbo_Extensions.TabIndex = 2
+        '
+        'cbo_lbl_Extensions
+        '
+        Me.cbo_lbl_Extensions.AutoSize = True
+        Me.cbo_lbl_Extensions.Location = New System.Drawing.Point(23, 21)
+        Me.cbo_lbl_Extensions.Name = "cbo_lbl_Extensions"
+        Me.cbo_lbl_Extensions.Size = New System.Drawing.Size(160, 17)
+        Me.cbo_lbl_Extensions.TabIndex = 3
+        Me.cbo_lbl_Extensions.Text = "Select Picture Extension"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.cbo_lbl_Extensions)
+        Me.Controls.Add(Me.cbo_Extensions)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btn_BrowseFolder)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Revert-ScanSpeeder"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btn_BrowseFolder As Button
     Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cbo_Extensions As ComboBox
+    Friend WithEvents cbo_lbl_Extensions As Label
 End Class
--- Revert-ScanSpeeder/Form1.vb
+++ Revert-ScanSpeeder/Form1.vb
@@ -1,15 +1,37 @@
-﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        ' Call ShowDialog.
-        Dim result As DialogResult = FolderBrowserDialog1.ShowDialog()
+﻿Imports System.IO
 
-        ' Test result.
-        If result = Windows.Forms.DialogResult.OK Then
-            ' Do something.
-            Dim path As String = FolderBrowserDialog1.SelectedPath
-            'Dim image As Bitmap = New Bitmap(Drawing.Image.FromFile(path))
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btn_BrowseFolder.Click
+
+        If cbo_Extensions.SelectedItem = "" Then
+            MsgBox("Please select an extension first!")
+        Else
+            FindFolderDialog()
         End If
     End Sub
 
+    Private Sub FindFolderDialog()
+        Dim result As DialogResult = FolderBrowserDialog1.ShowDialog()
+        If result = Windows.Forms.DialogResult.OK Then
+            Dim path As String = FolderBrowserDialog1.SelectedPath
+
+            Dim di As New DirectoryInfo(path)
+
+            ' Get a reference to each file in that directory.
+            Dim fiArr As FileInfo() = di.GetFiles()
+
+            ' Display the names of the files.
+            Dim fri As FileInfo
+
+            'initiate looping sequence
+            For Each fri In fiArr
+                If fri.Extension.Equals(cbo_Extensions.SelectedItem) Then
+                    Label1.Text += fri.Extension
+                End If
+            Next fri
+            'Dim image As Bitmap = New Bitmap(Drawing.Image.FromFile(path))
+        End If
+    End Sub
 
 End Class
